396d00fcfe0a81e438bee491d0dc40d93520c881
Joffrey Capitaine <joffrey.capitaine@gmail.com>
2018-03-18 14:31:36 +0100

    Smooth scroll tricks

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,18 @@ var $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
 require('bootstrap-sass');
+
+$(document).ready(function() {
+    function slideToTricks() {
+        $("#show-tricks").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".tricks-wrapper").offset().top
+            }, 2000);
+        });
+    }
+
+    /** Used functions **/
+    slideToTricks();
+});
+
+

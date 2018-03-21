bbc5b104731ca98ce8e399ac3614bd01a50d6b39
Joffrey Capitaine <joffrey.capitaine@gmail.com>
2018-03-22 00:11:32 +0100

    Begin form builder Trick

--- src/AppBundle/Controller/AddTrickController.php
+++ src/AppBundle/Controller/AddTrickController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Trick;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AddTrickController extends Controller
 {
@@ -14,8 +19,27 @@ class AddTrickController extends Controller
      */
     public function viewAddAction()
     {
+        // On crée un objet Trick
+        $trick = new Trick();
+
+        // On crée le FormBuilder grâce au service form factory
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $trick);
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $formBuilder
+            ->add('name',      TextType::class)
+            ->add('description',     TextType::class)
+            ->add('category',   ChoiceType::class)
+            ->add('save',      SubmitType::class)
+        ;
+
+        // À partir du formBuilder, on génère le formulaire
+        $form = $formBuilder->getForm();
+
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
         return $this->render('@App/AddTrick/view_add.html.twig', array(
-            // ...
+            'form' => $form->createView(),
         ));
     }
 

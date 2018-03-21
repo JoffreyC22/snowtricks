a96b099447c50bed72e986fbbb441c62180ab19a
Joffrey Capitaine <joffrey.capitaine@gmail.com>
2018-03-21 23:44:13 +0100

    Virtual host enfin en place

--- src/AppBundle/DataFixtures/ORM/LoadMedia.php
+++ src/AppBundle/DataFixtures/ORM/LoadMedia.php
@@ -35,7 +35,7 @@ class LoadMedia extends Fixture implements OrderedFixtureInterface
         $array = [];
         for ($i = 1; $i <= 10; $i++) {
             $array[] = [
-                'url' => '/snowtricks/web/uploads/images/tricks/trick'.$i.'.jpg',
+                'url' => '/uploads/images/tricks/trick'.$i.'.jpg',
                 'type' => 'image',
                 'trick' => 'trick'.$i,
                 'user' => 'joffrey'

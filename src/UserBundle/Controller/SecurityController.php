ef1b02d1973dc71d53aacb0aaf33557328cba97b
Joffrey Capitaine <joffrey@yeswedev.fr>
2018-04-13 16:09:06 +0200

    Begin SecurityController

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->redirectToRoute('oc_platform_accueil');
+        }
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        return $this->render('OCUserBundle:Security:login.html.twig', array(
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        ));
+    }
+}

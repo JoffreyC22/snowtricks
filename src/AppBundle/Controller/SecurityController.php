f0a284fcd40c1470fc412724cd3c490c1f587e04
Joffrey Capitaine <joffrey.capitaine@gmail.com>
2018-05-01 18:12:19 +0200

    Remove useless UserBundle

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace UserBundle\Controller;
+namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +15,7 @@ class SecurityController extends Controller
 
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        return $this->render('@User/Security/login.html.twig', array(
+        return $this->render('@App/Security/login.html.twig', array(
             'last_username' => $authenticationUtils->getLastUsername(),
             'error'         => $authenticationUtils->getLastAuthenticationError(),
         ));
